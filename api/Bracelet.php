bacf33606523e02c915c9921192f6f09d29ca4c6
zada-ai <sohaibmehmood0067@gmail.com>
2025-12-17 19:34:21 +0500

    2nd commit

--- api/Bracelet.php
+++ api/Bracelet.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
